490d91b23b1db753992d0fadb2c38ff0c215c7ad
Bradley Rispone <bradrispone@gmail.com>
2023-07-24 10:19:43 -0500

    Add skeleton of code for contacts page, just needs content now

--- src/contactpage.js
+++ src/contactpage.js
@@ -1,5 +1,19 @@
 const loadContact = () => {
+    const container = document.createElement('div');
+    container.id = "contact-container";
 
+    const contact = document.createElement('div');
+    contact.id = "contact-box";
+    contact.classList.add("info-box");
+    contact.innerHTML = "Get in touch!";
+
+    container.appendChild(contact);
+
+    const content = document.getElementById("content");
+    if(content.lastChild) {
+        content.removeChild(content.lastChild);
+    }
+    content.appendChild(container);
 };
 
 export { loadContact };
